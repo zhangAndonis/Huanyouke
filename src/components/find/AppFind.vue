f189a1d488bf7820cb487725b24fe459071fe579
zhangAandonis <1786867622@163.com>
2017-12-28 18:00:03 +0800

    index

---
+++ src/components/find/AppFind.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div class="app-find ">
+    <h1>好</h1>
+  </div>
+</template>
+
+<script>
+// import router from './router'
+export default {
+  name:'app-find'
+}
+</script>
+
+<style lang="scss">
+
+</style>
