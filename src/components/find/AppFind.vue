b7e4459e3d65e15232a2132d1c8ce5931226b867
zhangAandonis <1786867622@163.com>
2017-12-31 20:15:01 +0800

    router1

--- src/components/find/AppFind.vue
+++ src/components/find/AppFind.vue
@@ -2,7 +2,65 @@
   
   <div class="app-find ">
       <app-footer ></app-footer>  
-    <h1>发现</h1>
+      <div class="find-header">
+           <h1>发现</h1>
+      </div>
+      <div class="find-nav">
+         <ul>
+          <li>查攻略</li>
+          <li>写游记</li>
+          <li>问大家</li>
+          <li>记账单</li>
+         </ul>
+      </div>
+      <div class="find-destioation">
+          <div class="find-top">
+            <h1>最热旅途</h1>
+            <b></b>
+            <span>发现最美目的地</span>
+          </div>
+          <div class="find-con">
+              <div class="find-con1"></div>
+              <div class="find-con1"></div>
+              <div class="find-con1"></div>
+          </div>
+          <div class="find-title">
+            <h1>记录旅途中最美的瞬间</h1>
+          </div>
+           <div class="find-title">
+            <b>点击查看更多详情</b><span>更多</span>
+          </div>
+           <div class="find-title1">
+            <div class="find-left">
+              <span>更多</span>
+              <h2>旅行随手拍</h2>
+            </div> 
+            <div class="find-left">
+              <span>头像</span>
+              <h2>旅行随手拍</h2>
+            </div> 
+          </div>
+          <div class="find-hot">
+              <div class="hot-answers">
+                <h1>热门回答</h1>
+              </div>
+               <div class="hot-answers1">
+                <h1>有没有人知道厦门有什么地方适合烧烤？</h1>
+              </div>
+              <div class="hot-answers2">
+                <h1>你好，厦门适合野外烧烤的地方有十三陵水库，白羊沟和温榆河边。</h1>
+              </div>
+              <div class="hot-answers3">
+                  <span>8920浏览</span>
+                  <b>5回答</b>
+              </div>
+              <div class="find-more">
+                <h1>查看更多</h1>
+                <span>更多</span>
+              </div>
+          </div>
+          
+      </div>
   </div>
 </template>
 
--- src/components/main/AppNearby.vue
+++ src/components/main/AppNearby.vue
@@ -3,7 +3,7 @@
     <div class="z-top">
     
         <span @click="skip('index')">返回</span>
-        <img src="http://gallery.youxiake.com/Public/Data/upload/201710/16/33171508146250.jpg"/>
+        <img src="http://gallery.youxiake.com/Public/Data/upload/productimg/201703/13/58c6a0b784e96.jpg?imageView2/0/h/440"/>
     </div>
     <div class="content">
         <div class="z-delicacy">
@@ -13,7 +13,7 @@
            </div>
            <div class="z-delicacy-con">
                 <div class="z-con1">
-                  <img src="http://gallery.youxiake.com/Public/Data/upload/productimg/201701/06/586f5eb12b0b6.jpg?imageView2/0/h/300">
+                  <img src="http://gallery.youxiake.com/Public/Data/upload/productimg/201703/13/58c6a0b784e96.jpg?imageView2/0/h/440">
                  
                   <div>
                       <b>味千拉</b>
@@ -21,7 +21,7 @@
                   </div>
                 </div>
                 <div class="z-con1">
-                  <img src="http://gallery.youxiake.com/Public/Data/upload/productimg/201701/06/586f5eb12b0b6.jpg?imageView2/0/h/300">
+                  <img src="http://gallery.youxiake.com/Public/Data/upload/productimg/201703/13/58c6a0b784e96.jpg?imageView2/0/h/440">
                  
                   <div>
                       <b>味千拉</b>
@@ -29,7 +29,7 @@
                   </div>
                 </div>
                 <div class="z-con1">
-                  <img src="http://gallery.youxiake.com/Public/Data/upload/productimg/201701/06/586f5eb12b0b6.jpg?imageView2/0/h/300">
+                  <img src="http://gallery.youxiake.com/Public/Data/upload/productimg/201703/13/58c6a0b784e96.jpg?imageView2/0/h/440">
                  
                   <div>
                       <b>味千拉</b>
@@ -37,6 +37,44 @@
                   </div>
                 </div>
            </div>
+
+
+
+           <div class="z-delicacy-top">
+               <div class="z-left"><h1>热门景点</h1></div>
+               <div class="z-right"><span>换一批</span></div>
+           </div>
+           <div class="z-delicacy-con2">
+                <div class="z-con2">
+                  <img src="http://gallery.youxiake.com/Public/Data/upload/productimg/201703/13/58c6a0b784e96.jpg?imageView2/0/h/440">
+                  <b>北京八达岭长城</b>
+                  <div><span>五星好评</span><b>999人点评</b></div>
+                  <span>距离43公里</span>
+                </div>
+                <div class="z-con2">
+                  <img src="http://gallery.youxiake.com/Public/Data/upload/productimg/201703/13/58c6a0b784e96.jpg?imageView2/0/h/440">
+                  <b>北京八达岭长城</b>
+                  <div><span>五星好评</span><b>999人点评</b></div>
+                  <span>距离43公里</span>
+                </div>
+                
+           </div>
+            <div class="z-delicacy-con2">
+                <div class="z-con2">
+                  <img src="http://gallery.youxiake.com/Public/Data/upload/productimg/201703/13/58c6a0b784e96.jpg?imageView2/0/h/440">
+                  <b>北京八达岭长城</b>
+                  <div><span>五星好评</span><b>999人点评</b></div>
+                  <span>距离43公里</span>
+                </div>
+                <div class="z-con2">
+                  <img src="http://gallery.youxiake.com/Public/Data/upload/productimg/201703/13/58c6a0b784e96.jpg?imageView2/0/h/440">
+                  <b>北京八达岭长城</b>
+                  <div><span>五星好评</span><b>999人点评</b></div>
+                  <span>距离43公里</span>
+                </div>
+                
+           </div>
+
         </div>
     </div>
   </div>
