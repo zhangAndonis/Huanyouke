1ae1e63928e0110b3d14fba5c9f156e7f76380d7
xyz5900 <1246583142@qq.com>
2018-01-03 12:17:46 +0800

    sdffd

---
+++ src/components/mine/AppForgetPassword.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app-forget-password">
+    <div class="u23-c">
+      
+    </div>
+    <div class="u63-c">
+        <div class="back3">返回</div>   
+        <h1>找回密码</h1> 
+    </div>
+    <form>
+        <div class="main23-c">
+            <p><input type="text" placeholder="请输入手机号码"/></p> 
+            <p><input type="text" placeholder="短信验证码"/><a href="#javascript">获取验证码</a></p> 
+            <p><input type="password" placeholder="设置密码 6-15位数字或字母密码"/></p> 
+            <button>确定</button> 
+        </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'app-forget-password'
+}
+</script>
+
+<style lang="scss">
+
+</style>
+
---
+++ src/components/mine/AppLoginRegister.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+  <div class="app-login-register">
+    <div class="u2-c">
+      
+    </div>
+    <div class="u6-c">
+        <div class="back">返回</div>   
+        <h1>环游客欢迎您</h1> 
+    </div>
+    <div class="main-c">
+        <div class="message-c">
+            <p>账号登录</p>
+            <p>短信验证码登录</p>
+        </div>
+        <form>
+            <p>账号 <input type="text" placeholder="手机号/注册邮箱"/></p>
+            <p>密码 <input type="password"/> <button>隐藏/显示密码</button></p>
+            <p><span>快速注册</span> <span>忘记密码？</span></p>
+            <button class="login-c">登录</button>
+        </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'app-login-register',
+  components:{
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
